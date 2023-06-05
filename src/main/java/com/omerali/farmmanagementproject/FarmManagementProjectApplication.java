4685a5c3546b968b3505e04b9aa70867b0ddf2c0
omeralitosun <omerttosu@gmail.com>
2023-06-05 18:56:03 +0300

    Updated as Maven project

---
+++ src/main/java/com/omerali/farmmanagementproject/FarmManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.omerali.farmmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmManagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omerali/farmmanagementproject/FarmManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omerali.farmmanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
